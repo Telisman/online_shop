d5eced04070caf67188f0f7b1d312824c9001a3d
Davor Telisman <davortelisman@gmail.com>
2023-09-04 22:55:27 +0200

    Add react.js to are django project

--- shop_products/urls.py
+++ shop_products/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from .views import ProductList, ProductDetail,react_view
 
 urlpatterns = [
     path('api/products/', ProductList.as_view(), name='product-list'),
     path('api/products/<str:pk>/', ProductDetail.as_view(), name='product-detail'),
+    path('react/', react_view, name='react_view'),
+
 ]
--- shop_products/views.py
+++ shop_products/views.py
@@ -1,6 +1,13 @@
 from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
+# views.py
+
+from django.shortcuts import render
+
+def react_view(request):
+    return render(request, 'index.html')
+
 
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
