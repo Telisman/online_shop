481f8f01e025e226adab15c1cdeb3f9dd3d5fd7c
Davor Telisman <davortelisman@gmail.com>
2023-09-05 15:48:35 +0200

    Edit view.py to display product_list.html and product_detail.html page

--- shop_products/urls.py
+++ shop_products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ProductList, ProductDetail
+from . import views
+
 
 urlpatterns = [
-    path('api/products/', ProductList.as_view(), name='product-list'),
-    path('api/products/<str:pk>/', ProductDetail.as_view(), name='product-detail'),
-
+    path('api/products/', views.ProductList.as_view(), name='product-list'),
+    path('api/products/<str:pk>/', views.ProductDetail.as_view(), name='product-detail'),
+    path('products/', views.product_list, name='product_list'),
+    path('products/<str:product_id>/', views.product_detail, name='product_detail'),
 ]
--- shop_products/views.py
+++ shop_products/views.py
@@ -1,13 +1,26 @@
 from rest_framework import generics
-from .models import Product
 from .serializers import ProductSerializer
-# views.py
-
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 
+# app product list
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'product_detail.html', {'product': product})
 
+
+
+
+
+
+
+
+
+# API BACKEND ENDPOINT VIEW FUNCTION
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
