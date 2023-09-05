214ad026cabaa214761ae66d52bd3be53d18aefc
Davor Telisman <davortelisman@gmail.com>
2023-09-05 22:44:07 +0200

    Add css and js to are html page

--- shop_products/urls.py
+++ shop_products/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('api/products/', views.ProductList.as_view(), name='product-list'),
     path('api/products/<str:pk>/', views.ProductDetail.as_view(), name='product-detail'),
-    path('products/', views.product_list, name='product_list'),
+    path('', views.product_list, name='product_list'),
     path('products/<str:product_id>/', views.product_detail, name='product_detail'),
 ]
